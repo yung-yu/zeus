41e8027ef6435a67d2346d7c1da692de30b46d50
andy <yungyu405828@gmail.com>
2017-06-10 17:33:59 +0800

    update to do list

--- js/public.js
+++ js/public.js
@@ -1,25 +1,52 @@
 /**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2016 OA Wu Design
+ * @author     Andy Li<yungyu405728@gmail.com>
+ * @copyright   Copyright (c) 2016 Andy Li Design
  */
 
 $(function () {
-	$('#button').click(function(){
-		console.log('checked');
-		if($('.box').hasClass('checked')){
-			$('.box').removeClass('checked');
-		} else {
+	var count = 0;
+	$('#send').click(function(){
+		 var text = $('#edite').val();
+		 if(text != ''){
+			 var changeColorBtn = $('<label />').addClass('changeColor');
+			 var repairBtn = $('<label />').addClass('repair');
+			 var deleteBtn = $('<label />').addClass('delete');
+			$('#main .content').append(
+				$('<div />').addClass('item').append($('<label />').addClass('itemValue').text(text))
+				.append(
+				$('<div />').addClass('tools')
+				.append(changeColorBtn.text('變色'))
+				.append(repairBtn.text('修改'))
+				.append(deleteBtn.text('刪除'))
+				));
+			$('#edite').val('');
 
-			$('.box').addClass('checked');
+			changeColorBtn.click(function(){
+		    	if($(this).parent().parent().hasClass('change')){
+		    		console.log('remove');
+		        	$(this).parent().parent().removeClass('change');
+		        } else {
+		        	console.log('add');
+					$(this).parent().parent().addClass('change');
+		        }
+		    });
+
+		   repairBtn.click(function(){
+		    	console.log('repair');
+		    	var newText = prompt('請輸入新的內容', '');
+	            if(newText){
+	            	console.log(newText);
+	            	$(this).parent().parent().children('.itemValue').text(newText);
+
+	            }
+		    	
+		    });
+
+		   deleteBtn.click(function(){
+		    	$(this).parent().parent().remove();
+		    });
 		}
 	});
 
-	$('#enter').click(function(){
-		console.log('get text'+$('#in').val());
-		var $text = $('#in').val();
-		if($text != ''){
-	    	$('#items').append($('<div/>').text($text));
-	    }
-	    $('#in').val('');
-	});
+    
 });
